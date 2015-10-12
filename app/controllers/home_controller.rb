6e884eda02207c06011171aed2607d109a26d75f
Seung Min <bsm7878@naver.com>
2015-10-12 10:35:51 +0000

    controller modify

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -120,20 +120,7 @@ class HomeController < ApplicationController
   
   
   def write_save
-    uri = URI("http://eee.jnu.ac.kr/user/indexSub.action?codyMenuSeq=13342&siteId=eee&menuUIType=top")
-    html_doc = Nokogiri::HTML(Net::HTTP.get(uri))
-    i= 0 
-      while i < 8
-        professors = Professor.new
-        img_sub = html_doc.css("div.professor_img // img")[i].attr('src')
-        professors.img = "http://eee.jnu.ac.kr//" + img_sub
-        professors.name = html_doc.css("dl.professor_dl1 // strong")[i].inner_text
-        professors.space = html_doc.css("dl.professor_dl2 // dd")[i].inner_text
-        professors.phone = html_doc.css("dl.professor_dl3 // dd")[i].inner_text
-        professors.email = html_doc.css("dl.professor_dl4 // dd")[i].inner_text
-        professors.save
-        i = i + 1
-      end
+    
   end
   
 
